f854ad7d84f59226efb6be9f8b8c98d3f71f7f28
xiaoyao <marcus.lu.sg@gmail.com>
2026-07-09 11:27:08 -0700

    Make demographic form fields mandatory except feedback
    Add HTML5 required attribute to Age, Gender, Race, Ethnicity,
    Religious status, and Religion inputs so the browser blocks
    submission until each is answered. The feedback textarea stays
    optional. Also fixed the Religion <select> so its placeholder option
    is disabled/non-selectable (value=''), since required validation
    otherwise passes on the unselected placeholder having a truthy value.

--- js/demographic_form.js
+++ js/demographic_form.js
@@ -20,57 +20,57 @@ const demographic_form = {
         '<div style="text-align:left; display: inline-block;' +
         ' line-height:1.8em;">' +
         // age text box
-        '<input name="age" type="number"  min="18" max="100"/><br><br>' +
+        '<input name="age" type="number"  min="18" max="100" required/><br><br>' +
         // gender options
         '<input name="gender" type="radio" id="woman" value=' +
-        '"Woman" /> <label for="woman"> Woman </label>' +
+        '"Woman" required/> <label for="woman"> Woman </label>' +
         '<input name="gender" type="radio" id="man" value=' +
-        '"Man" /> <label for="man"> Man </label>' +
+        '"Man" required/> <label for="man"> Man </label>' +
         '<input name="gender" type="radio" id="nonbinary" value=' +
-        '"Non-binary" /> <label for="nonbinary"> Non-binary </label> <br>' +
+        '"Non-binary" required/> <label for="nonbinary"> Non-binary </label> <br>' +
         '<input name="gender" type="radio" id="other_gender" value=' +
-        '"other_gender" /> <label for="other_gender"> Other: <input' +
+        '"other_gender" required/> <label for="other_gender"> Other: <input' +
         ' type="text" name="other_gender" /> </label><br><br><br>' +
         // race options
         '<input name="race" type="radio" id="white" value=' +
-        '"White" /> <label for="white"> White </label> <br>' +
+        '"White" required/> <label for="white"> White </label> <br>' +
         '<input name="race" type="radio" id="black" value=' +
-        '"Black/African American" /> <label for="black">' +
+        '"Black/African American" required/> <label for="black">' +
         ' Black/African American </label> <br>' +
         '<input name="race" type="radio" id="am_ind" value=' +
-        '"American Indian/Alaska Native" /> <label for="am_ind">' +
+        '"American Indian/Alaska Native" required/> <label for="am_ind">' +
         ' American Indian/Alaska Native </label> <br>' +
         '<input name="race" type="radio" id="asian" value=' +
-        '"Asian" /> <label for="asian"> Asian </label> <br>' +
+        '"Asian" required/> <label for="asian"> Asian </label> <br>' +
         '<input name="race" type="radio" id="pac_isl" value=' +
-        '"Native Hawaiian/Pacific Islander" /> <label for="pac_isl">' +
+        '"Native Hawaiian/Pacific Islander" required/> <label for="pac_isl">' +
         ' Native Hawaiian/Pacific Islander </label> <br>' +
-        '<input name="race" type="radio" id="other_race" value="other_race" />' +
+        '<input name="race" type="radio" id="other_race" value="other_race" required/>' +
         '<label for="other_race"> Other: <input type="text"' +
         'name="other_race" /> </label><br><br>' +
         // ethnicity options
         '<input name="ethnicity" type="radio" id="hisp" value=' +
-        '"Hispanic" /> <label for="hisp"> Hispanic </label>' +
+        '"Hispanic" required/> <label for="hisp"> Hispanic </label>' +
         '<input name="ethnicity" type="radio" id="nonhisp" value=' +
-        '"Non-Hispanic" /> <label for="nonhisp"> Non-Hispanic' +
+        '"Non-Hispanic" required/> <label for="nonhisp"> Non-Hispanic' +
         ' </label><br><br>' +
         // religious status
         '<input name="religious" type="radio" id="not at all" value=' +
-        '"Not at all" /> <label for="not at all"> Not at all </label> <br>' +
+        '"Not at all" required/> <label for="not at all"> Not at all </label> <br>' +
         '<input name="religious" type="radio" id="slightly" value=' +
-        '"Slightly" /> <label for="slightly">' +
+        '"Slightly" required/> <label for="slightly">' +
         ' Slightly </label> <br>' +
         '<input name="religious" type="radio" id="moderately" value=' +
-        '"Moderately" /> <label for="moderately">' +
+        '"Moderately" required/> <label for="moderately">' +
         ' Moderately </label> <br>' +
         '<input name="religious" type="radio" id="very" value=' +
-        '"Very" /> <label for="very"> Very </label> <br>' +
+        '"Very" required/> <label for="very"> Very </label> <br>' +
         '<input name="religious" type="radio" id="extremely" value=' +
-        '"Extremely" /> <label for="extremely">' +
+        '"Extremely" required/> <label for="extremely">' +
         ' Extremely </label><br><br>' +
         // religion type
-        '<select id="religion" name="religion"><br>' +
-        '<option value="Please choose an option">--Please choose an option--</option>' +
+        '<select id="religion" name="religion" required><br>' +
+        '<option value="" disabled selected>--Please choose an option--</option>' +
         '<option value="Protestant Christian">Protestant Christian</option>' +
         '<option value="Catholic Christian">Catholic Christian</option>' +
         '<option value="Orthodox Christian">Orthodox Christian</option>' +
